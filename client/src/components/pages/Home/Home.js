7067c4f363d848814808bf7c767bbab0e687b4fb
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-03-03 13:50:06 +0100

    Home page hero done. TODO: last section of home page

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,6 +1,35 @@
+import {Container, Row, Col} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import './Home.css'
+
+
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <>
+            <section className='hero'>
+                <Container className='d-flex align-items-center justify-content-center'>
+                    <Row className='justify-content-center'>
+                        <Col>
+                            <h1>Welcome to Nutrinfo</h1>
+                            <h5>Get all the info you need from your own recipes!</h5>  
+                            <Link className='btn btn-white btn-transparent' to='/signup'>Signup!</Link>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            <section className='more-info'>
+                <Row>
+                    <Col className='community'>
+                        <h2>Check out all the recipes from our community</h2>
+                    </Col>
+
+                    <Col>
+                        <h2>Don't know what goes here yet</h2>
+                    </Col>
+
+                </Row>
+            </section>
+        </>
     )
 }
 
--- client/src/service/recipes.service.js
+++ client/src/service/recipes.service.js
@@ -2,19 +2,15 @@ import axios from 'axios'
 
 class RecipeService {
 
-    constructor() {
-        
+    constructor(){
         this.api = axios.create({
-            baseULR: `${process.env.REACT_APP_API_URL}/recipes`,
+            baseURL: `${process.env.REACT_APP_WHATEVER}/recipes`,
             withCredentials: true
-        })
-    }
+    })}
 
-    createRecipe = newRecipe => this.api.post('/createRecipe', newRecipe).catch(err => console.log(err))
-    getRecipes = () => {
-        console.log(this.api)
-        return this.api.get('/getAllRecipes').catch(err => console.log(err))
-    }
+
+    createRecipe = newRecipe => this.api.post('/createRecipe', newRecipe)
+    getRecipes = () => this.api.get(`/getAllRecipes`)
     getRecipe = recipeId => this.api.get(`/getOneRecipe/${recipeId}`).catch(err => console.log(err))
     getUserRecipes = userId => this.api.get(`/getRecipesByUser/${userId}`).catch(err => console.log(err))
     deleteRecipe = recipeId => this.api.delete(`/deleteRecipe/${recipeId}`).catch(err => console.log(err))
