3c7c4d7e1ff2bbf72d06ad8bac325670ddca6e89
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-03-07 20:34:06 +0100

    bug solvingç

--- client/src/components/pages/Recipe-details/Doughnut-chart.js
+++ client/src/components/pages/Recipe-details/Doughnut-chart.js
@@ -38,7 +38,7 @@ class DoughnutChart extends Component {
                 ctx.font = fontSize + "em sans-serif";
                 ctx.textBaseline = "middle";
           
-                const text = props.data[0].quantity,
+                const text = Math.round(props.data[0].quantity),
                     textX = Math.round((width - ctx.measureText(text).width) / 2),
                     textY = height / 2 ;
                 ctx.fillText(text, textX, textY);
