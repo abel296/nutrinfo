72c21c7b225d73a3d230261717702a75b76b9359
Abel Andrés <abelandres.gtc@gmail.com>
2023-04-24 10:01:06 +0200

    ✨ add confirmation before removing recipe

--- client/src/components/pages/Profile/UserRecipeCard.js
+++ client/src/components/pages/Profile/UserRecipeCard.js
@@ -1,38 +1,62 @@
 import { Link } from 'react-router-dom'
-import { Col, Card, Button } from 'react-bootstrap'
+import { Col, Card, Button, Modal } from 'react-bootstrap'
 import './UserRecipeCard.css'
 
 import RecipeService from '../../../service/recipes.service'
+import { Component } from 'react'
 
-const UserRecipeCard = ({image, title, _id, refreshList, handleAlert}) => {
-
-    const recipeService = new RecipeService()
-
-    const deleteUserCard = () => {
-
-        recipeService
-            .deleteRecipe(_id)
-            .then(() => refreshList())
-            .catch(err => handleAlert(true, 'Error', 'Error removing recipe'))
+class UserRecipeCard extends Component {
+    constructor() {
+        super()
+        this.state = {
+            showDeleteRecipe: false
+        }
     }
 
+    deleteUserCard = () => {
+        const recipeService = new RecipeService()
 
-    return (
-        <Col lg={4} md={6} xs={12}>
-            <Card className='user-card'>
-            <div className='card-image-wrapper'>
-                <Card.Img className='card-image' variant="top" src={image?.url}/>
-            </div>
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <div className='profile-btn'>
-                        <Link to={`/recipe-details/${_id}`} className='btn profile-details-btn'>See details</Link>
-                        <Button onClick={() => deleteUserCard()} className='profile-details-btn delete-btn'>Delete Recipe</Button>
-                    </div>
-                </Card.Body>
-            </Card>
-        </Col>
+        recipeService
+            .deleteRecipe(this.props._id)
+            .then(() => this.props.refreshList())
+            .catch(err => this.props.handleAlert(true, 'Error', 'Error removing recipe'))
+    }
+
+    togglemodalDelete(value) {
+        this.setState({ showDeleteRecipe: value })
+    }
+
+    render() {
+        return (
+            <>
+                <Col lg={ 4 } md={ 6 } xs={ 12 }>
+                    <Card className='user-card'>
+                        <div className='card-image-wrapper'>
+                            <Card.Img className='card-image' variant="top" src={ this.props.image?.url } />
+                        </div>
+                        <Card.Body>
+                            <Card.Title>{ this.props.title }</Card.Title>
+                            <div className='profile-btn'>
+                                <Link to={ `/recipe-details/${ this.props._id }` } className='btn profile-details-btn'>See details</Link>
+                                <Button onClick={ () => this.togglemodalDelete(true) } className='profile-details-btn delete-btn'>Delete Recipe</Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Modal show={ this.state.showDeleteRecipe } onHide={ () => this.togglemodalDelete(false) }>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Are you sure?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className='d-flex justify-content-between'>
+                        <Button className='btn delete-btn' onClick={ () => this.deleteUserCard() }>Delete Recipe Permanently</Button>
+                        <Button className='btn' onClick={ () => this.togglemodalDelete(false) }>Cancel</Button>
+
+                    </Modal.Body>
+                </Modal>
+            </>
         )
+    }
 }
 
 export default UserRecipeCard
