f3cadd73d246d70017bd6a37d43c4b976bf1004b
Abel Andrés <abelandres.gtc@gmail.com>
2023-04-24 09:54:48 +0200

    ✨ adjust session storage

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -3,16 +3,24 @@ const bcrypt = require("bcrypt")
 const passport = require("passport")
 const LocalStrategy = require("passport-local").Strategy
 const flash = require("connect-flash")
+const MongoStore = require("connect-mongo")
 
 const User = require('../models/user.model')
 
 module.exports = app => {
+    process.env.ENVIRONMENT === 'production' && app.set('trust proxy', 1)
 
     app.use(session({
         secret: process.env.SESSION_SECRET,
-        resave: true,
-        saveUninitialized: true,
-
+        cookie: process.env.ENVIRONMENT === 'production'
+            ? { secure: true, sameSite: 'none' }
+            : {},
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI,
+            ttl: 24 * 60 * 60,
+            resave: false,
+            saveUninitialized: false
+        })
     }))
 
     passport.serializeUser((user, next) => next(null, user._id))
